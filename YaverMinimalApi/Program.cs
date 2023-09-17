2dfe0ea4c14fe428a46784157685eca2bc3f0c45
gurolg <gurol@gurkan.org>
2023-09-17 17:46:56 +0300

    refactor: folder structure

--- YaverMinimalApi/Program.cs
+++ YaverMinimalApi/Program.cs
@@ -18,8 +18,26 @@ var app = builder.Build();
 app.UseAuthorization();
 // app.UseAuthentication();
 
-app.UseFastEndpoints();
+app.UseFastEndpoints(c =>
+{
+    c.Serializer.Options.PropertyNamingPolicy = System.Text.Json.JsonNamingPolicy.CamelCase;
 
+    c.Errors.ResponseBuilder = (failures, ctx, statusCode) =>
+    {
+        return new Microsoft.AspNetCore.Mvc.ValidationProblemDetails(
+            failures.GroupBy(f => f.PropertyName)
+                    .ToDictionary(
+                        keySelector: e => e.Key,
+                        elementSelector: e => e.Select(m => m.ErrorMessage).ToArray()))
+        {
+            Type = "https://tools.ietf.org/html/rfc7231#section-6.5.1",
+            Title = "One or more validation errors occurred.",
+            Status = statusCode,
+            Instance = ctx.Request.Path,
+            Extensions = { { "traceId", ctx.TraceIdentifier } }
+        };
+    };
+});
 using (var Scope = app.Services.CreateScope())
 {
     var context = Scope.ServiceProvider.GetRequiredService<ApiDbContext>();
