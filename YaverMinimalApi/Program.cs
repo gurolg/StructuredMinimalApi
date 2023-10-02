883994ebf78f3e73e41cfc5f06d95c74d865b964
gurolg <gurol@gurkan.org>
2023-10-02 15:28:24 +0300

    fastendpoints JsonEnumConverter

--- YaverMinimalApi/Program.cs
+++ YaverMinimalApi/Program.cs
@@ -49,10 +49,12 @@ builder.Services
 var app = builder.Build();
 
 app
-	.UseOpisExceptionHandler(logStructuredException:true)
+	.UseOpisExceptionHandler(logStructuredException: true)
 	.UseAuthentication()
 	.UseAuthorization()
-	.UseFastEndpoints(c => {
+	.UseFastEndpoints(c =>
+	{
+		c.Serializer.Options.Converters.Add(new JsonStringEnumConverter());
 		// c.Errors.UseProblemDetails();
 		c.Endpoints.Configurator = (ep) => { ep.PreProcessors(Order.Before, new MyRequestLogger()); };
 		//https://fast-endpoints.com/docs/configuration-settings#customizing-error-responses
@@ -72,7 +74,8 @@ app
 	});
 
 
-app.MapRemote("http://localhost:6000", c => {
+app.MapRemote("http://localhost:6000", c =>
+{
 	c.ChannelOptions.MaxRetryAttempts = 3;
 	//c.ChannelOptions.HttpHandler = new() { ... };
 	//c.ChannelOptions.ServiceConfig = new() { ... };
@@ -92,9 +95,11 @@ app.MapRemote("http://localhost:6000", c => {
 app.Run();
 
 
-public class MyRequestLogger : IGlobalPreProcessor {
+public class MyRequestLogger : IGlobalPreProcessor
+{
 	public async Task PreProcessAsync(object req, HttpContext ctx, List<ValidationFailure> failures,
-		CancellationToken ct) {
+		CancellationToken ct)
+	{
 		await Task.CompletedTask;
 		// var logger = ctx.RequestServices.GetRequiredService<ILogger>();
 		// logger.LogInformation($"request:{req?.GetType().FullName} path: {ctx.Request.Path}");
